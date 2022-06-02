19a9163c53a7cd9f705958b1f3883e098ba4532f
Valerio Cipolla <83750862+ValerioCipolla@users.noreply.github.com>
2022-06-02 12:04:06 +0100

    Time: 144 ms (12.33%), Space: 14.6 MB (90.58%) - LeetHub

--- 867-transpose-matrix/867-transpose-matrix.py
+++ 867-transpose-matrix/867-transpose-matrix.py
@@ -1,7 +1,10 @@
-import numpy as np
-
-
 class Solution:
     def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
-        matrix = np.array(matrix)
-        return matrix.transpose()
+        res = []
+        pos = 0
+        while pos < len(matrix[0]):
+            res.append([])
+            for i in range(len(matrix)):
+                res[pos].append(matrix[i][pos])
+            pos += 1
+        return res
